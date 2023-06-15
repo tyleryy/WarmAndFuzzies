2fcf43c4665069d88921c12d720fdc3c840e042c
Tyler Yu <tyler.y.yu@gmail.com>
2023-06-15 03:03:55 -0700

    switched grid layout order

--- src/app/user-pages/checklist/[id]/grid_layout.tsx
+++ src/app/user-pages/checklist/[id]/grid_layout.tsx
@@ -13,20 +13,23 @@ const GridLayout = ({item_list}: any) => {
     return (
         <div className='grid grid-cols-3 gap-5 m-3'>
             <div className='flex flex-col space-y-3'>
-                {item_list.slice(0, item_list.length/3).map((elem: userMessage, index: number) => {
+                {item_list.slice(2*item_list.length/3).map((elem: userMessage, index: number) => {
                     return <Message user={elem.user} message={elem.message} key={index}/>
                 })}
             </div>
+            
             <div className='flex flex-col space-y-3'>
                 {item_list.slice(item_list.length/3, 2*(item_list.length/3)).map((elem: userMessage, index: number) => {
                     return <Message user={elem.user} message={elem.message} key={index}/>
                 })}
             </div>
+
             <div className='flex flex-col space-y-3'>
-                {item_list.slice(2*item_list.length/3).map((elem: userMessage, index: number) => {
+                {item_list.slice(0, item_list.length/3).map((elem: userMessage, index: number) => {
                     return <Message user={elem.user} message={elem.message} key={index}/>
                 })}
             </div>
+        
             
         </div>
     )
